a410713d81d121778f8018edd66f902c9b442cf9
paolabr4 <90522319+paolabr4@users.noreply.github.com>
2022-07-30 07:59:47 -0500

    Llenado y muestra de matriz aleatorio

--- src/arregloBidimencional.java
+++ src/arregloBidimencional.java
@@ -1,3 +1,18 @@
 public class arregloBidimencional {
+    public static void main(String[] args) {
+        int[][] numeros= new int [4][5];
+        for (int  i = 0; i < numeros.length;i++){
+            for (int j=0; j < numeros[i].length;j++){
+                numeros[i][j]=(int)Math.floor(Math.random()*(9 - 1));
+            }
+        }
+
+        for (int i=0; i< numeros.length; i++){
+            for (int j=0;j<numeros[i].length;j++){
+                System.out.print(numeros[i][j]+" ");
+            }
+            System.out.println("");
+        }
+    }
 
 }
